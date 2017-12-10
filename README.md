5fa423803349fd8baf282ca177f9f1923be7c263
Mikael Söder <mik@elsoder.se>
2017-12-10 19:17:54 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# sopel-trafikverket
+
+A Sopel module which pulls information from the open API provided by swedish Trafikverket
---
+++ sopel_modules/trafikverket/__init__.py
@@ -0,0 +1,13 @@
+# coding=utf8
+"""sopel-trafikverket
+
+A Sopel module which pulls information from the open API provided by swedish Trafikverket
+"""
+from __future__ import unicode_literals, absolute_import, division, print_function
+
+from .trafikverket import *
+
+__author__ = 'Mikael Söder'
+__email__ = 'mik@elsoder.se'
+__version__ = '0.1.0'
+
